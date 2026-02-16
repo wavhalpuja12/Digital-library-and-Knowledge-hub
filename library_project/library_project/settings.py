a31a4a655c2b47c225c67e7eb6de988ee180ef90
puja wavhal <poojawavhal2012@gmail.com>
2026-02-16 16:14:15 +0530

    responsive pages

--- library_project/library_project/settings.py
+++ library_project/library_project/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'digital_library_db',
         'USER': 'root',
-        'PASSWORD': 'pran@253',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
