4f28f751af5e625df0b7ef3b29d24105cae4ddfc
saishende29 <saishende29@gmail.com>
2026-02-25 16:26:52 +0530

    Add Form Blogs

--- library_project/library_app/urls.py
+++ library_project/library_app/urls.py
@@ -43,4 +43,8 @@ urlpatterns = [
     
     # path('delete-category/<int:id>/', views.delete_category, name='delete_category')
 
+    #blog pages
+    path('blog_contend',views.blog_contend,name='blog_contend'),
+    path('blog_galaxy',views.blog_galaxy,name='blog_galaxy')
+
 ]
